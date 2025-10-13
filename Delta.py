8765cd9a52c5b654dcb6cfbd654ec6d1dacfae24
johnjezl <john@jezl.com>
2025-10-12 17:38:42 -0700

    Merge comment changes

--- Delta.py
+++ Delta.py
@@ -3,6 +3,22 @@ from buildStates import *
 
 class Delta:
 
+
+
+    """
+    Input:  
+        state - the state to start from (base 8 encoding representing a string of characters)
+        input - the character to transition on
+    Output: 
+        new_state - the new state a state of less than 5 chars will be in after transitioning on input
+        or state - the state a state of over 5 chars will be in after transitioning on input
+        or failed_state - a state of -1 representing there was no valid transition from (state, input)
+    Example:
+        Input - delta(1, a)
+        Output - 9 (using base 8 encoding)
+    Preconditions: 
+        failed_state and alphabet must be defined, state must be a valid state representing a string in the alphabet or failed state
+    """
     @staticmethod
     def delta(state, input):
         if state == failed_state:
@@ -20,6 +36,20 @@ class Delta:
             else:
                 return failed_state
 
+
+
+    """
+    Input:  
+        state - the state to start from (representing a string of characters)
+        input - the character to transition on
+    Output: 
+        true if is valid transition (if adding input to state results in all characters in alphabet), false if not
+    Example:
+        Input - _is_valid_transition(115785, a)     (where 115785 = cdbaaa in base 8)
+        Output - false
+    Preconditions: 
+        state must represent a string of at least 5 chars, input must be a letter that exists in alphabet
+    """
     @staticmethod
     def _is_valid_transition(state, input):
         input_index = alphabet.index(input) + 1
--- buildDFA.py
+++ buildDFA.py
@@ -3,6 +3,19 @@ from Alphabet import alphabet
 from Delta import *
 from buildStates import buildStates
 
+
+
+"""
+Input:  
+    None
+Output: 
+    A DFA containing states, an alphabet, delta function, start state of 0, and accepting states
+Example:
+    input - buildDFA()
+    output - A DFA containing the above
+Preconditions:
+    buildStates(), Delta(), and alphabet must be defined
+"""
 def buildDFA():
     states, accepting_states = buildStates()
     return DFA(states, alphabet, Delta(), 0, accepting_states)
