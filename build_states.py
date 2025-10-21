0b3dcf0ce261931a98679120be2f2c0e4f6c36f2
LKurosana <163683237+LKurosana@users.noreply.github.com>
2025-10-20 17:24:30 -0700

    Update function comments

--- build_states.py
+++ build_states.py
@@ -8,16 +8,15 @@ failed_state = -1
 
 """
 Input:
-    letters_to_add - number of letters to add to state
-    state - current state being build off of
+    max_length - the max length of a state
 Output:
     states - list of all possible states, including the failed state
     accepting_states - list of all possible accepting states
 Example:
-    input - buildStates(5, 0) with alphabet of 4 chars
+    input - buildStates(5)
     output - list of 1365 states (plus failed_state) and list of 1365 accepting states
 Preconditions:
-    buildStatesHelper and failed_state must be defined, letters_to_add must be a positive int
+    failed_state must be defined
 """
 def build_states(max_length = 5):
     states = [0]
@@ -29,6 +28,20 @@ def build_states(max_length = 5):
     return list(states), list(accepting_states)
 
 
+
+"""
+Input:
+    current - the current state being built up
+    max_length - The max length of a state (5)
+    states - the place to store the built states
+Output:
+    None
+Example:
+    call - generate_state_numbers("", 5, states)
+    result - All combinations of states up to length 5 have been generated and stores in 'states'
+Preconditions:
+    max_length >= 1
+"""
 def generate_state_numbers(current, max_length, states):
     """Recursively generate all combinations up to max_length"""
     if len(current) > 0:
